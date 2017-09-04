fe192e99d66c2319a9918302c10b9fe721dd0c89
Park, Yunha <hadbsgk@gmail.com>
2017-09-04 16:23:29 +0900

    set external library and css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BigtoNavComponent } from './bigto-nav/bigto-nav.component';
+import { BigtoFooterComponent } from './bigto-footer/bigto-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BigtoNavComponent,
+    BigtoFooterComponent
   ],
   imports: [
     BrowserModule
