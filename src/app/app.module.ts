b211177e21e7538fe05da945fbfb47cd34d94e91
Park, Yunha <hadbsgk@gmail.com>
2017-09-04 20:11:30 +0900

    implement basic line graph for average price per date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppComponent } from './app.component';
 import { BigtoNavComponent } from './bigto-nav/bigto-nav.component';
 import { BigtoFooterComponent } from './bigto-footer/bigto-footer.component';
 import { BigtoGraphComponent } from './bigto-graph/bigto-graph.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MdSelectModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { BigtoService } from "./shared/bigto.service";
 
 @NgModule({
   declarations: [
@@ -14,9 +19,13 @@ import { BigtoGraphComponent } from './bigto-graph/bigto-graph.component';
     BigtoGraphComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MdSelectModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BigtoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/bigto-graph/bigto-graph.component.ts
+++ src/app/bigto-graph/bigto-graph.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {Chart} from 'chart.js';
+import {BigtoService} from "../shared/bigto.service";
+import {Spec} from "../core/models/spec";
+
 @Component({
   selector: 'app-bigto-graph',
   templateUrl: './bigto-graph.component.html',
@@ -8,43 +11,99 @@ import {Chart} from 'chart.js';
 export class BigtoGraphComponent implements OnInit {
   @ViewChild('myChart') myChart;
 
-  constructor() { }
+  mySpec: Spec;
+
+  krmodels: Array<string>;
+  gbs: Array<string>;
+  conditions: Array<string>;
+  changes: Array<string>;
+
+  config = {
+    type: 'line',
+    data: {
+      labels: ["8/10", "8/11", "8/12", "8/13", "8/14", "8/15"],
+      datasets: [{
+        type: 'line',
+        label: 'price of X',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      title:{
+        display:true,
+        text:'X'
+      },
+      scales: {
+        xAxes: [{
+          display: true,
+        }],
+        yAxes: [{
+          display: true,
+        }]
+      },
+      annotation: {
+        events: ['click'],
+      }
+    }
+  }
+
+  constructor(private bigtoService: BigtoService) { }
 
   ngOnInit() {
-    this.myChart = new Chart(this.myChart.nativeElement, {
-      type: 'bar',
-      data: {
-        labels: ["1", "2", "3", "4", "5"],
-        datasets: [{
-          label: '# of card in use',
-          data: [12,13,14,15,16],
-          backgroundColor: 'rgba(255, 99, 132, 0.2)',
-          borderColor: 'rgba(255,99,132,1)',
-          borderWidth: 2
-        },
-          {
-            label: '# of Remaining card',
-            data: [2,3,4,5,6],
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 2
-          }],
-      },
-      options: {
-        scales: {
-          xAxes: [{
-            stacked: true
-          }],
-          yAxes: [{
-            ticks: {
-              beginAtZero:true
-            },
-            stacked: true,
-          }],
-        },
-        responsive: true
-      }
+    this.mySpec = new Spec();
+
+    this.bigtoService.getKrmodels().subscribe(data => {
+      this.krmodels = data['krnameList'];
+      console.log(data);
     });
+
+    if (this.mySpec.krmodel != null) {
+      this.bigtoService.getGbs(this.mySpec.krmodel).subscribe(data => {
+        this.gbs = data['gbList'];
+        console.log(data);
+      });
+    }
+
+    this.myChart = new Chart(this.myChart.nativeElement, this.config);
+  }
+
+  add() {
+    this.bigtoService.getAveragePricePerDay(this.mySpec).subscribe(data => {
+      this.config.data.labels = data['labels'];
+      this.config.data.datasets[0].data = data['data'];
+
+      this.myChart.update();
+    });
+  }
+
+  onSelectKrmodel() {
+    this.bigtoService.getGbs(this.mySpec.krmodel).subscribe(data => {
+      this.gbs =  data['gbList'];
+      console.log(this.gbs);
+    });
+  }
+
+  onSelectGb() {
+    this.bigtoService.getConditions(this.mySpec.krmodel, this.mySpec.gb).subscribe(data => {{
+      this.conditions = data['conditionsList'];
+      console.log(this.conditions);
+    }});
+  }
+
+  onSelectConditions() {
+    this.bigtoService.getChanges(this.mySpec.krmodel, this.mySpec.gb, this.mySpec.conditions)
+      .subscribe(data => {
+        this.changes = data['changesList'];
+        console.log(this.changes);
+      });
   }
 
 }
